e66ffc104d1acc6b5183538f660344bc4bf21cdb
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2015-06-08 21:21:04 +0200

    Added better input support

--- voxel/util.js
+++ voxel/util.js
@@ -24,3 +24,41 @@ function drawFullscreenQuad(shader) {
     // Draw 6 vertices => 2 triangles:
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
+
+var keysDown = new Array();
+var keysPressed = new Array();
+var keysPressedLast = new Array();
+document.onkeydown = function(e) {
+    // Make more beautiful checks
+    if (keysDown.indexOf(e.keyCode) === -1) {
+        keysDown.push(e.keyCode);
+    }
+    if (keysPressed.indexOf(e.keyCode) === -1 && keysPressedLast.indexOf(e.keyCode) === -1) {
+        keysPressed.push(e.keyCode);
+    }
+}
+document.onkeyup = function(e) {
+    var index = keysDown.indexOf(e.keyCode);
+    if (index > -1) {
+        keysDown.splice(index, 1);
+    }
+    
+    index = keysPressedLast.indexOf(e.keyCode);
+    if (index > -1) {
+        keysPressedLast.splice(index, 1);
+    }
+}
+
+function isKeyPressed(key) {
+    var index = keysPressed.indexOf(key);
+    
+    if (index > -1) {
+        keysPressed.splice(index, 1);
+        keysPressedLast.push(key);
+        return true;
+    }
+    return false;
+}
+function isKeyDown(key) {
+    return keysDown.indexOf(key) > -1;
+}
