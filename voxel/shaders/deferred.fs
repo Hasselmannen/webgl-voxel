9641d9ab3d3b9ca82b2a66a6e60ddb89bbe5197e
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2016-02-09 14:32:28 +0100

    Moved SSAO to separate shaders

--- voxel/shaders/deferred.fs
+++ voxel/shaders/deferred.fs
@@ -4,18 +4,12 @@ varying vec2 texCoord;
 varying vec3 farPlaneRay;
 
 uniform vec3 viewSpaceLightPos;
-uniform mat4 projectionMatrix;
 uniform mat4 invModelViewMatrix;
-uniform mat4 invProjectionMatrix;
 
 uniform sampler2D normalDepthTexture;
 uniform sampler2D diffuseTexture;
 uniform samplerCube shadowMap;
-
-#define ENABLE_SSAO
-#define NUM_SSAO_SAMPLES 64
-uniform sampler2D noiseTexture;
-uniform vec3 hemisphere[NUM_SSAO_SAMPLES];
+uniform sampler2D ssaoTexture;
 
 vec3 ambientLight = vec3(0.3, 0.3, 0.3);
 vec3 diffuseLight = vec3(0.9, 0.9, 0.5);
@@ -95,35 +89,7 @@ void main(void) {
         }
     }
 
-    // SSAO Implementation (http://john-chapman-graphics.blogspot.co.il/2013/01/ssao-tutorial.html)
-    // TODO: Move scaling with render target size to uniforms
-    // TODO: Move to separate shader for blur passes
-    // TODO: Move radius to uniform
-    vec3 randomVec = texture2D(noiseTexture, texCoord * vec2(1280.0/4.0, 720.0/4.0)).xyz;
-    vec3 tangent = normalize(randomVec - normal * dot(randomVec, normal));
-    vec3 bitangent = cross(normal, tangent);
-    mat3 invTBN = mat3(tangent, bitangent, normal);
-
-    float occlusion = 0.0;
-    #ifdef ENABLE_SSAO
-    float radius = 0.75;
-    for (int i = 0; i < NUM_SSAO_SAMPLES; i++) {
-        vec3 sample = invTBN * hemisphere[i];
-        sample = sample * radius + viewSpacePosition;
-
-        vec4 offset = vec4(sample, 1.0);
-        offset = projectionMatrix * offset;
-        offset.xy /= offset.w;
-        offset.xy = offset.xy * 0.5 + 0.5;
-
-        float sampleDepth = -texture2D(normalDepthTexture, offset.xy).a;
-
-        float rangeCheck = abs(viewSpacePosition.z - sampleDepth) < radius ? 1.0 : 0.0;
-        occlusion += (sampleDepth > sample.z ? 1.0 : 0.0) * rangeCheck;
-    }
-    occlusion /= float(NUM_SSAO_SAMPLES);
-    #endif
-    occlusion = 1.0 - occlusion;
+    vec3 occlusion = texture2D(ssaoTexture, texCoord).rgb;
 
     gl_FragColor = vec4(vec3(0)
     + ambient * vec3(occlusion)
