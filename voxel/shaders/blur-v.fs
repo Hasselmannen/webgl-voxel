3018d18ab76a42cdde0f21e1686e808cdab3393c
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2016-02-14 01:37:04 +0100

    More refactoring (size uniforms in blur shaders, and more general cubeMapTextures/framebuffers)

--- voxel/shaders/blur-v.fs
+++ voxel/shaders/blur-v.fs
@@ -1,16 +1,17 @@
 precision highp float;
 
 uniform sampler2D texture;
+uniform float sampleResY;
 
 varying vec2 texCoord;
 
 void main(void) {
     vec3 color =
-        0.122581 * texture2D(texture, texCoord - vec2(0, 2.0/720.0)).rgb +
-        0.233062 * texture2D(texture, texCoord - vec2(0, 1.0/720.0)).rgb +
+        0.122581 * texture2D(texture, texCoord - vec2(0, 2.0/sampleResY)).rgb +
+        0.233062 * texture2D(texture, texCoord - vec2(0, 1.0/sampleResY)).rgb +
         0.288713 * texture2D(texture, texCoord).rgb +
-        0.233062 * texture2D(texture, texCoord + vec2(0, 1.0/720.0)).rgb +
-        0.122581 * texture2D(texture, texCoord + vec2(0, 2.0/720.0)).rgb;
+        0.233062 * texture2D(texture, texCoord + vec2(0, 1.0/sampleResY)).rgb +
+        0.122581 * texture2D(texture, texCoord + vec2(0, 2.0/sampleResY)).rgb;
 
     gl_FragColor = vec4(color, 1);
 }
--- voxel/util.js
+++ voxel/util.js
@@ -18,13 +18,10 @@ function drawFullscreenQuad(shader) {
     var screenQuadVBO = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuadVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(fullscreenQuadVertices), gl.STATIC_DRAW);
-
-    // Bind:
-    gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuadVertexBuffer);
+    
     gl.enableVertexAttribArray(shader.attributes["position"]);
     gl.vertexAttribPointer(shader.attributes["position"], 2, gl.FLOAT, false, 0, 0);
 
-    // Draw 6 vertices => 2 triangles:
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
